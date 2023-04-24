754a0b6bb0d68e9523b7881c8d3b3830fdcebf4a
psyjm24 <psyjm24@nottingham.ac.uk>
2023-04-24 16:38:23 +0100

    Add Chap 1 & 2

---
+++ chap1.hs
@@ -0,0 +1,46 @@
+--
+
+-- Exercise 1
+
+{-
+   double (double 2)
+   double (2 + 2)
+   2+2 + 2+2
+   4 + 2+2
+   4 + 4
+   8
+-}
+
+-- Exercise 2
+
+sum :: Num a => [a] -> a
+sum [] = 0
+sum (x:xs) = Main.sum xs + x
+
+{-
+   Using definition of sum above:
+   sum [x]
+   sum [] + x
+   0 + x
+   x
+   Therefore sum [x] = x for all x
+-}
+
+-- Exercise 3
+
+product :: Num a => [a] -> a
+product [] = 1
+product (x:xs) = Main.product xs * x
+
+-- Exercise 4
+
+qsortRev :: Ord a => [a] -> [a]
+qsortRev [] = []
+qsortRev (x:xs) = qsortRev bigger ++ [x] ++ qsortRev smaller
+               where
+                smaller = [a | a <- xs, a <= x]
+                bigger = [b | b <- xs, b > x]
+
+-- Exercise 5
+
+-- Replacing <= in the definition of qsort would mean that any duplicates of the selected x would be ignored for the sorted version of that list.
---
+++ chap2.hs
@@ -0,0 +1,46 @@
+--Example 1
+
+-- Example 2
+
+{-
+
+1. 2^(3*4)
+2. (2*3)+(4*5)
+3. 2+(3*(4^5))
+
+-}
+
+-- Example 3
+
+n = a `div` length xs
+    where
+        a = 10
+        xs = [1,2,3,4,5]
+
+{-
+
+Changes made:
+'N' changed to 'n'
+'div' changed to `div`
+a and xs indented the same
+
+-}
+
+-- Example 4
+
+last1 :: [a] -> a
+last1 = head . reverse
+
+last2 :: [a] -> a
+last2 xs = head(drop (length xs - 1) xs)
+
+last3 :: [a] -> a
+last3 xs = xs !! (length xs - 1)
+
+--Example 5
+
+init1 :: [a] -> [a]
+init1 xs = reverse(drop 1(reverse xs))
+
+init2 :: [a] -> [a]
+init2 xs = take (length xs - 1) xs
